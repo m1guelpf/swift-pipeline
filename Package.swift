1aa738c66caf03f1f18a2f6f0e4fda735a1581a9
Miguel Piedrafita <github@miguel.build>
2025-02-03 11:58:11 +0100

    wip

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "Pipeline",
     platforms: [
-        .macOS(.v11),
         .iOS(.v16),
-        .tvOS(.v13),
-        .watchOS(.v6),
+        .tvOS(.v16),
+        .macOS(.v13),
+        .watchOS(.v9),
     ],
     products: [
         .library(name: "Pipeline", type: .dynamic, targets: ["Pipeline"]),
